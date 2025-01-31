3ab46b2cdaf597f0d33a4c380268e5f7db703032
codot-spec <exform.arc@gmail.com>
2025-01-31 21:22:07 +0530

    7th commit

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+//const User = require('./models/user');
 
 const app = express();
 
@@ -18,20 +20,24 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('679bd2afce29d6f503847abe')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('679bd2afce29d6f503847abe')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+mongoose.connect('mongodb+srv://codotspec:databasekapass@cluster0.3dxan.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0')
+.then(result => {
   app.listen(3000);
+})
+.catch(err => {
+  console.log(err);
 });
